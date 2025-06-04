d7e71515a8029e617d43f8bdb464b34ad122f03c
Pup Meat <zdalepayne@gmail.com>
2025-06-04 05:21:08 -0500

    D: Player triangle added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.time import Clock
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -10,11 +11,14 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player1.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000.0
 
