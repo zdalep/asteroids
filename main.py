9fd695e7f0139a9599a07f1047714a78343c5a1a
Pup Meat <zdalepayne@gmail.com>
2025-06-04 04:25:28 -0500

    B: FPS limit added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.time import Clock
 from constants import *
 
 def main():
@@ -7,13 +8,15 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000.0
 
 
 if __name__ == "__main__":
